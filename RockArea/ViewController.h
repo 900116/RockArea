e22c94e1ea7267b23e0a73bcd3737d4ed21cdb89
zgh <zhaoguanghui@yongche.com>
2015-03-25 11:02:24 +0800

    Initial Commit

---
+++ RockArea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RockArea
+//
+//  Created by YongCheHui on 15/3/25.
+//  Copyright (c) 2015年 YongCheHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RockArea/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RockArea
+//
+//  Created by YongCheHui on 15/3/25.
+//  Copyright (c) 2015年 YongCheHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
